5289f307d3513715784477f3615f8ccdda2e8172
Thobani <mjiyakhothobani@gmail.com>
2024-07-16 15:48:16 +0200

    string_with_most_vowels

--- string_with_most_vowels.py
+++ string_with_most_vowels.py
@@ -1,6 +1,6 @@
 def string_with_most_vowels(words):
     def count_vowels(word):
-        return sum(1 for letter in word if letter in {'aeiou'})
+        return sum(1 for letter in word if letter in 'aeiou')
     
     most_vowels_count = 0
     word_with_most_vowels = None
